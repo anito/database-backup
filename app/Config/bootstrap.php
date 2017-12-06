bebfb42c7e62355edfb721d9990adb4fd1cc20b2
anito <axelnitzschner@mac.com>
2017-12-06 04:59:30 +0100

    test values

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -222,7 +222,7 @@ function c($max = 5) {
     App::uses('File', 'Utility');
     $files = l(SORT_ASC, TRUE);
     var_dump($files);
-    var_dump(min($files));
+    var_dump(key($files));
     die;
     if(count($files) > $max) {
         $file = new File(min($files));
