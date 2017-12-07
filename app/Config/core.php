7acd1c797329462c4330e85d4c9a07e7892058cf
anito <axelnitzschner@mac.com>
2017-12-07 02:13:30 +0100

    Localization de_DE

--- app/Config/core.php
+++ app/Config/core.php
@@ -18,8 +18,8 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
-//setLocale(LC_ALL, 'deu');
-//Configure::write('Config.language', 'deu');
+        setLocale(LC_ALL, 'de_DE');
+//        Configure::write('Config.language', 'deu');
 
 /**
  * CakePHP Debug Level:
