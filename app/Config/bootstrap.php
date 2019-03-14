c0a6f24cb4f4110fbae09487e66d726d047fadcf
anito <axelnitzschner@mac.com>
2019-03-14 03:48:55 +0100

    config changes

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -181,7 +181,7 @@ if (!defined('FLASH_JSON')) {
     define('FLASH_JSON', '/Elements/flash_json');
 }
 if (!defined('IMAGES_CONFIG_URL')) {
-    define('IMAGES_CONFIG_URL', IMAGES . DS . 'config');
+    define('IMAGES_CONFIG_URL', IMAGES . 'config');
 }
 if (!defined('SALT')) {
     define('SALT', 'urrasjksdjkbsdakbjvgikjbgfiabrg');
